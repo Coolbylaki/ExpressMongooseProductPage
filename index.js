089683350f9573454337b0a59a0d59f2aea11e67
Lazar Stojanović <lakisd98@gmail.com>
2022-10-24 12:51:59 +0200

    Add product details page route

--- index.js
+++ index.js
@@ -18,9 +18,15 @@ app.set("view engine", "ejs");
 
 app.get("/products", async (req, res) => {
     const products = await Product.find({});
-    res.render("products/index", { products })
-})
+    res.render("products/index", { products });
+});
+
+app.get("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("products/show", { product });
+});
 
 app.listen(port, () => {
     console.log(`App is listening on port: ${port}`);
-})
+});
